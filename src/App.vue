50854748f92eba53ff6812505f84ba16e99712ef
mikhail2013ru <mikhail2013ru@gmail.com>
2019-04-01 19:02:21 +0400

    First Vue Project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<v-app>
+	<Navbar/>
+</v-app>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld'
+import Navbar from './components/Navbar.vue'
+
+export default {
+	components: {
+		Navbar
+	}
+}
+</script>
